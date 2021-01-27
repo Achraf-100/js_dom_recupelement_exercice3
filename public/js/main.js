a83b01e5e1a733cbd2b536b4aff7a1c064f3b6be
Achraf <hardy1348@live.fr>
2021-01-27 14:18:57 +0100

    fin

---
+++ public/js/main.js
@@ -0,0 +1,32 @@
+// let div1 = document.getElementsByTagName('div')[2]
+// let enfantDeDiv = div1.firstElementChild
+// console.log(enfantDeDiv);
+
+// // // 2 
+// let dernierEnfant = div1.lastElementChild;
+// console.log(dernierEnfant);
+
+
+// let elem = div1.getElementsByTagName('p')[0];
+// let elemI = elem.firstElementChild;
+// console.log(elemI);
+
+// // 4 
+// let p = div1.lastElementChild; 
+// console.log(p.firstElementChild);
+
+// // 4 (methode 2 )
+// let b = document.getElementsByTagName('b')[0]; 
+// console.log(b); 
+
+// // 5 
+// let i = div1.getElementsByTagName('i')[0]; 
+// console.log(i.parentElement); // parentNode; 
+
+// // 6 
+// console.log(b.parentNode); 
+
+// // 7 
+// console.log(enfantDeDiv.nextElementSibling); 
+
+
